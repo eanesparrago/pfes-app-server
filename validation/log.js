365a69b761925508cebd9b71cb0d6429b171ebc3
LJEsp <esparragoleandroj@gmail.com>
2018-08-08 23:32:02 +0800

    Add max character validation to logs

--- validation/log.js
+++ validation/log.js
@@ -10,6 +10,27 @@ module.exports = function validateLogInput(data) {
     errors.domJo = "DOM/JO# is required and must be unique";
   }
 
+  if (!Validator.isLength(data.shipperConsignee, { max: 100 })) {
+    errors.shipperConsignee =
+      "Shipper/Consignee must not exceed 100 characters";
+  }
+
+  if (!Validator.isLength(data.commodity, { max: 100 })) {
+    errors.commodity = "Commodity must not exceed 100 characters";
+  }
+
+  if (!Validator.isLength(data.blAwb, { max: 100 })) {
+    errors.blAwb = "BL/AWB must not exceed 100 characters";
+  }
+
+  if (!Validator.isLength(data.origin, { max: 100 })) {
+    errors.origin = "Origin must not exceed 100 characters";
+  }
+
+  if (!Validator.isLength(data.destination, { max: 100 })) {
+    errors.destination = "Destination must not exceed 100 characters";
+  }
+
   return {
     errors: errors,
     isValid: isEmpty(errors)
